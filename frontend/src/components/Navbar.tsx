ea405e6acd483167971c31ab300a76adb997c51a
chh59ja <christian.hauptmann@uni-wuerzburg.de>
2024-11-04 18:11:55 +0100

    adjust navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,26 +1,78 @@
-// src/components/Navbar.tsx
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, useMediaQuery } from '@mui/material';
+import React, { useState } from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    IconButton,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    useMediaQuery,
+    ListItemButton
+} from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import SportsVolleyballIcon from '@mui/icons-material/SportsVolleyball';
+import HistoryIcon from '@mui/icons-material/History';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const Navbar: React.FC = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open);
+  };
+
+  const drawerContent = (
+    <List>
+      <ListItemButton component={Link} to="/" onClick={toggleDrawer(false)}>
+        <ListItemIcon><SportsVolleyballIcon /></ListItemIcon>
+        <ListItemText primary="Aktuelle Turniere" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/tournaments" onClick={toggleDrawer(false)}>
+        <ListItemIcon><HistoryIcon /></ListItemIcon>
+        <ListItemText primary="Vergangene Turniere" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/create-tournament" onClick={toggleDrawer(false)}>
+        <ListItemIcon><AddCircleIcon /></ListItemIcon>
+        <ListItemText primary="Turnier erstellen" />
+      </ListItemButton>
+    </List>
+  );
 
   return (
-    <AppBar position="static" style={{marginBottom: "2em"}}>
+    <AppBar position="static" style={{ marginBottom: "2em" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Beachvolleyball Turnier
+          BeachL Turnier App
         </Typography>
         {isMobile ? (
-          <Button color="inherit">Menu</Button> // Hier könnte später ein Drawer integriert werden
+          <>
+            <IconButton color="inherit" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+              {drawerContent}
+            </Drawer>
+          </>
         ) : (
           <>
-            <Button color="inherit" component={Link} to="/">Home</Button>
-            <Button color="inherit" component={Link} to="/tournaments">Turniere</Button>
+            <Button color="inherit" component={Link} to="/">
+              <SportsVolleyballIcon sx={{ mr: 1 }} />
+              Aktuelle Turniere
+            </Button>
+            <Button color="inherit" component={Link} to="/tournaments">
+              <HistoryIcon sx={{ mr: 1 }} />
+              Vergangene Turniere
+            </Button>
             <Button color="inherit" component={Link} to="/create-tournament">
+              <AddCircleIcon sx={{ mr: 1 }} />
               Turnier erstellen
             </Button>
           </>
